73fc700f77c51e3b8a1c9c914aff1c081bc6bc43
girvel <widauka@ya.ru>
2025-03-23 14:51:30 +0500

    Read and display 10 characters (closes #3)

--- boot.asm
+++ boot.asm
@@ -1,7 +1,12 @@
 ; boot.asm
 [org 0x7c00]        ; Tell the assembler where this code will be loaded
 
-mov ah, 0x0e        ; BIOS teletype function
+; Clean the screen
+mov ah, 0x00  ; Set video mode
+mov al, 0x03  ; 80x25 text mode
+int 0x10
+
+mov ah, 0x0E        ; BIOS teletype function
 
 mov al, 'L'
 int 0x10
@@ -33,6 +38,27 @@ mov al, '.'
 int 0x10
 mov al, '1'
 int 0x10
+
+mov al, 0x0D
+int 0x10
+mov al, 0x0A
+int 0x10
+mov al, 0x0D
+int 0x10
+mov al, 0x0A
+int 0x10
+
+mov al, '!'
+int 0x10
+
+mov cx, 10
+
+rw_loop:
+    mov ah, 0x00  ; Read keypress
+    int 0x16
+    mov ah, 0x0E
+    int 0x10
+    loop rw_loop
 
 hlt                 ; Halt the CPU
 
