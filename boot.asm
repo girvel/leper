300dcfae60b939e0d6a518ac160f2e4bee164903
girvel <widauka@ya.ru>
2025-03-25 13:01:54 +0500

    Failed attempt at loading the kernel + 32 bit

--- boot.asm
+++ boot.asm
@@ -5,9 +5,21 @@ mov ah, 0x00  ; Set video mode
 mov al, 0x03  ; 80x25 text mode
 int 0x10
 
-cli
-lgdt [gdt_descriptor]
+; Load the kernel into memory at 0x1000
+mov ah, 0x02
+mov al, 5
+mov ch, 0
+mov cl, 2
+mov dh, 0
+mov dl, 0
+mov bx, 0x1000
+int 0x13
+jc disk_error
 
+cli  ; Disable BIOS interrupts
+lgdt [gdt_descriptor]  ; Load GDT
+
+; Enable protected mode
 mov eax, cr0
 or eax, 0x1
 mov cr0, eax
@@ -16,6 +28,7 @@ jmp 0x08:protected_mode
 
 [bits 32]
 protected_mode:
+    ; Set up segment registers
     mov ax, 0x10
     mov ds, ax
     mov es, ax
@@ -23,16 +36,8 @@ protected_mode:
     mov gs, ax
     mov ss, ax
 
+    ; Point stack to arbitrary address
     mov esp, 0x9000
-
-    mov edi, 0xB8000
-    mov ah, 0x0F
-    mov al, 'H'
-    mov [edi], ax
-    add edi, 2
-    mov al, 'i'
-    mov [edi], ax
-    hlt
 
 gdt_start:
     ; Null descriptor
@@ -60,56 +65,9 @@ gdt_end:
 gdt_descriptor:
     dw gdt_end - gdt_start - 1  ; Limit
     dd gdt_start                ; Base
- 
-; mov ah, 0x0E  ; BIOS teletype function
-; 
-; mov al, 'L'
-; int 0x10
-; mov al, 'e'
-; int 0x10
-; mov al, 'p'
-; int 0x10
-; mov al, 'e'
-; int 0x10
-; mov al, 'r'
-; int 0x10
-; mov al, ' '
-; int 0x10
-; mov al, 'O'
-; int 0x10
-; mov al, 'S'
-; int 0x10
-; mov al, ' '
-; int 0x10
-; mov al, 'v'
-; int 0x10
-; mov al, '0'
-; int 0x10
-; mov al, '.'
-; int 0x10
-; mov al, '0'
-; int 0x10
-; mov al, '.'
-; int 0x10
-; mov al, '1'
-; int 0x10
 
-; mov ah, 0x02
-; mov al, 5
-; mov ch, 0
-; mov cl, 2
-; mov dh, 0
-; mov dl, 0
-; mov bx, 0x1000
-; int 0x13
-; jc disk_error
-; jmp 0x1000
-; 
-; disk_error:
-;     mov ah, 0x0E
-;     mov al, '?'
-;     int 0x10
-;     hlt
+disk_error:
+    hlt
 
 times 510-($-$$) db 0  ; Fill the rest of the boot sector with zeros
 dw 0xaa55  ; Boot signature
--- kernel.asm
+++ kernel.asm
@@ -1,3 +1,9 @@
-mov ah, 0x0E
-mov al, '!'
-int 0x10
+[bits 32]
+mov edi, 0xB8000
+mov ah, 0x0F
+mov al, 'H'
+mov [edi], ax
+add edi, 2
+mov al, 'i'
+mov [edi], ax
+hlt
