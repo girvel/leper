81062250443dfbff70a317477e8abb70b110836b
girvel <widauka@ya.ru>
2025-03-26 18:27:08 +0500

    Generated bootloader (3)

--- boot.asm
+++ boot.asm
@@ -2,27 +2,26 @@
 [bits 16]
 
 start:
-    ; Set up stack
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0x7C00
 
-    ; Load kernel sectors (2-6) to 0x10000 (1MB)
-    mov ah, 0x02    ; BIOS read sectors
-    mov al, 5       ; Number of sectors to read
-    mov ch, 0       ; Cylinder 0
-    mov cl, 2       ; Starting sector (2)
-    mov dh, 0       ; Head 0
-    mov dl, 0       ; Drive 0 (floppy)
-    mov bx, 0x1000  ; ES:BX = 0x0000:0x1000 (0x10000)
+    ; Load kernel to 0x10000 (1MB)
+    mov ah, 0x02
+    mov al, 5       ; Sectors to read
+    mov ch, 0       ; Cylinder
+    mov cl, 2       ; Sector
+    mov dh, 0       ; Head
+    mov dl, 0       ; Drive
+    mov bx, 0x1000  ; ES:BX = 0x1000:0x0000 (0x10000)
     mov es, bx
     xor bx, bx
     int 0x13
     jc disk_error
 
-    ; Switch to protected mode
+    ; Switch to 32-bit
     cli
     lgdt [gdt32_desc]
     mov eax, cr0
@@ -32,67 +31,54 @@ start:
 
 [bits 32]
 protected_mode:
-    ; Set up segment registers
     mov ax, 0x10
     mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
     mov ss, ax
     mov esp, 0x90000
 
-    ; Set up PAE and page tables
+    ; Set up 64-bit paging
     call setup_paging
 
-    ; Switch to long mode
-    mov ecx, 0xC0000080  ; EFER MSR
+    ; Enable long mode
+    mov ecx, 0xC0000080
     rdmsr
-    or eax, 0x00000100   ; LME bit
+    or eax, 0x100
     wrmsr
 
     mov eax, cr0
-    or eax, 0x80000000   ; PG bit
+    or eax, 0x80000000
     mov cr0, eax
 
     jmp 0x08:long_mode
 
 setup_paging:
-    ; Set up PML4, PDPT, PD tables
-    mov edi, 0x70000     ; Physical address for PML4
-    mov cr3, edi         ; Set CR3 while still in 32-bit mode
-
-    ; Clear memory for page tables
+    mov edi, 0x1000
+    mov cr3, edi
     xor eax, eax
-    mov ecx, 0x4000      ; 16KB (4 tables)
+    mov ecx, 4096
     rep stosd
+    mov edi, 0x1000
 
-    ; Set up PML4
-    mov edi, 0x70000
-    mov dword [edi], 0x71003  ; Point to PDPT (present + writable)
-
-    ; Set up PDPT
-    mov edi, 0x71000
-    mov dword [edi], 0x72003  ; Point to PD (present + writable)
-
-    ; Set up PD (identity map first 2MB using 2MB pages)
-    mov edi, 0x72000
-    mov dword [edi], 0x00000083  ; First 2MB (present + writable + huge page)
-    mov dword [edi+8], 0x200083  ; Next 2MB (present + writable + huge page)
-
+    ; PML4
+    mov dword [edi], 0x2003
+    add edi, 0x1000
+    ; PDP
+    mov dword [edi], 0x3003
+    add edi, 0x1000
+    ; PD (2MB pages)
+    mov dword [edi], 0x00000083
+    mov dword [edi+8], 0x20000083
     ret
 
 [bits 64]
 long_mode:
-    ; Clear segment registers
     mov ax, 0
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
-
-    ; Jump to 64-bit kernel at 1MB
-    jmp 0x10000
+    jmp 0x10000  ; Jump to kernel
 
 disk_error:
     mov si, error_msg
@@ -109,16 +95,14 @@ print_string:
 .done:
     ret
 
-; 32-bit GDT
 gdt32:
-    dq 0x0000000000000000  ; Null descriptor
-    dq 0x00CF9A000000FFFF  ; Code segment (0x08)
-    dq 0x00CF92000000FFFF  ; Data segment (0x10)
+    dq 0
+    dq 0x00CF9A000000FFFF ; Code
+    dq 0x00CF92000000FFFF ; Data
 gdt32_desc:
     dw gdt32_desc - gdt32 - 1
     dd gdt32
 
 error_msg db "Disk error!", 0
-
 times 510-($-$$) db 0
 dw 0xAA55
--- kernel.asm
+++ kernel.asm
@@ -1,83 +1,31 @@
 [bits 64]
-[org 0x10000]  ; Loaded at 1MB by bootloader
+[org 0x10000]
 
 start:
-    ; Set up video memory pointer
-    mov rdi, 0xB8000  ; VGA text buffer address
-
-    ; Clear screen (optional)
-    mov rax, 0x0F200F200F200F20  ; Black background, white space chars
-    mov rcx, 500                 ; Clear 500 words (80x25 screen / 2)
+    ; Clear screen (write spaces)
+    mov rdi, 0xB8000
+    mov rax, 0x0F200F200F200F20 ; White-on-black spaces
+    mov rcx, 500
     rep stosq
 
-    ; Print "Hello World!"
-    mov rdi, 0xB8000  ; Reset to start of video memory
-    
-    ; Color attribute: white on black (0x0F)
-    mov ah, 0x0F
-    
-    ; H
-    mov al, 'H'
-    mov [rdi], ax
-    add rdi, 2
-    
-    ; e
-    mov al, 'e'
-    mov [rdi], ax
-    add rdi, 2
-    
-    ; l
-    mov al, 'l'
-    mov [rdi], ax
-    add rdi, 2
-    
-    ; l
-    mov al, 'l'
-    mov [rdi], ax
-    add rdi, 2
-    
-    ; o
-    mov al, 'o'
-    mov [rdi], ax
-    add rdi, 2
-    
-    ; Space
-    mov al, ' '
-    mov [rdi], ax
-    add rdi, 2
-    
-    ; W
-    mov al, 'W'
-    mov [rdi], ax
-    add rdi, 2
-    
-    ; o
-    mov al, 'o'
-    mov [rdi], ax
-    add rdi, 2
-    
-    ; r
-    mov al, 'r'
-    mov [rdi], ax
-    add rdi, 2
-    
-    ; l
-    mov al, 'l'
-    mov [rdi], ax
-    add rdi, 2
-    
-    ; d
-    mov al, 'd'
-    mov [rdi], ax
-    add rdi, 2
-    
-    ; !
-    mov al, '!'
-    mov [rdi], ax
+    ; Print message
+    mov rdi, 0xB8000
+    lea rsi, [rel msg]
+    mov ah, 0x0F ; White on black
 
-    ; Infinite loop
+.print_loop:
+    lodsb
+    test al, al
+    jz .done
+    mov [rdi], ax
+    add rdi, 2
+    jmp .print_loop
+
+.done:
     hlt
     jmp $
 
-; Pad to 5 sectors (2.5KB)
-times 2560 - ($ - $$) db 0
+msg: db "Hello 64-bit World!", 0
+
+; Pad to 5 sectors
+times 2560-($-$$) db 0
