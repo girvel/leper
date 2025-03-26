33c1ca5dde3c39dedfc968300186deb6e8aa124f
girvel <widauka@ya.ru>
2025-03-26 18:41:06 +0500

    Undo everything autogenerated, remain at 16 bits

--- boot.asm
+++ boot.asm
@@ -1,108 +1,59 @@
-[org 0x7C00]
-[bits 16]
+[org 0x7c00]  ; Tell the assembler where this code will be loaded
 
-start:
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    mov sp, 0x7C00
+; Clean the screen
+mov ah, 0x00  ; Set video mode
+mov al, 0x03  ; 80x25 text mode
+int 0x10
 
-    ; Load kernel to 0x10000 (1MB)
-    mov ah, 0x02
-    mov al, 5       ; Sectors to read
-    mov ch, 0       ; Cylinder
-    mov cl, 2       ; Sector
-    mov dh, 0       ; Head
-    mov dl, 0       ; Drive
-    mov bx, 0x1000  ; ES:BX = 0x1000:0x0000 (0x10000)
-    mov es, bx
-    xor bx, bx
-    int 0x13
-    jc disk_error
+mov ah, 0x0E  ; BIOS teletype function
 
-    ; Switch to 32-bit
-    cli
-    lgdt [gdt32_desc]
-    mov eax, cr0
-    or eax, 1
-    mov cr0, eax
-    jmp 0x08:protected_mode
+mov al, 'L'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'p'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'v'
+int 0x10
+mov al, '0'
+int 0x10
+mov al, '.'
+int 0x10
+mov al, '0'
+int 0x10
+mov al, '.'
+int 0x10
+mov al, '1'
+int 0x10
 
-[bits 32]
-protected_mode:
-    mov ax, 0x10
-    mov ds, ax
-    mov ss, ax
-    mov esp, 0x90000
-
-    ; Set up 64-bit paging
-    call setup_paging
-
-    ; Enable long mode
-    mov ecx, 0xC0000080
-    rdmsr
-    or eax, 0x100
-    wrmsr
-
-    mov eax, cr0
-    or eax, 0x80000000
-    mov cr0, eax
-
-    jmp 0x08:long_mode
-
-setup_paging:
-    mov edi, 0x1000
-    mov cr3, edi
-    xor eax, eax
-    mov ecx, 4096
-    rep stosd
-    mov edi, 0x1000
-
-    ; PML4
-    mov dword [edi], 0x2003
-    add edi, 0x1000
-    ; PDP
-    mov dword [edi], 0x3003
-    add edi, 0x1000
-    ; PD (2MB pages)
-    mov dword [edi], 0x00000083
-    mov dword [edi+8], 0x20000083
-    ret
-
-[bits 64]
-long_mode:
-    mov ax, 0
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    jmp 0x10000  ; Jump to kernel
+mov ah, 0x02
+mov al, 5
+mov ch, 0
+mov cl, 2
+mov dh, 0
+mov dl, 0
+mov bx, 0x1000
+int 0x13
+jc disk_error
+jmp 0x1000
 
 disk_error:
-    mov si, error_msg
-    call print_string
+    mov ah, 0x0E
+    mov al, '?'
+    int 0x10
     hlt
 
-print_string:
-    lodsb
-    or al, al
-    jz .done
-    mov ah, 0x0E
-    int 0x10
-    jmp print_string
-.done:
-    ret
-
-gdt32:
-    dq 0
-    dq 0x00CF9A000000FFFF ; Code
-    dq 0x00CF92000000FFFF ; Data
-gdt32_desc:
-    dw gdt32_desc - gdt32 - 1
-    dd gdt32
-
-error_msg db "Disk error!", 0
-times 510-($-$$) db 0
-dw 0xAA55
+times 510-($-$$) db 0  ; Fill the rest of the boot sector with zeros
+dw 0xaa55  ; Boot signature
--- kernel.asm
+++ kernel.asm
@@ -1,31 +1,3 @@
-[bits 64]
-[org 0x10000]
-
-start:
-    ; Clear screen (write spaces)
-    mov rdi, 0xB8000
-    mov rax, 0x0F200F200F200F20 ; White-on-black spaces
-    mov rcx, 500
-    rep stosq
-
-    ; Print message
-    mov rdi, 0xB8000
-    lea rsi, [rel msg]
-    mov ah, 0x0F ; White on black
-
-.print_loop:
-    lodsb
-    test al, al
-    jz .done
-    mov [rdi], ax
-    add rdi, 2
-    jmp .print_loop
-
-.done:
-    hlt
-    jmp $
-
-msg: db "Hello 64-bit World!", 0
-
-; Pad to 5 sectors
-times 2560-($-$$) db 0
+mov ah, 0x0E
+mov al, '!'
+int 0x10
