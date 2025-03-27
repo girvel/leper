af63f225a77337236c57cc202fcf0fe4276ba2f3
girvel <widauka@ya.ru>
2025-03-27 14:10:25 +0500

    Extract stdlib

---
+++ stdlib.asm
@@ -0,0 +1,63 @@
+writeln:  ; si: string pointer
+    call write
+    mov al, 13
+    int 0x10
+    mov al, 10
+    int 0x10
+    ret
+
+
+write:  ; si: string pointer
+    mov ah, 0x0E  ; BIOS teletype function
+.loop:
+    lodsb
+    test al, al
+    jz .done
+    int 0x10
+    jmp .loop
+.done:
+    ret
+
+
+readln:  ; di: string pointer, cx: length limit
+.loop:
+    test cx, cx
+    jz .done
+
+    mov ah, 0x00  ; BIOS blocking input function
+    int 0x16
+
+    mov ah, 0x0E  ; BIOS teletype function
+    int 0x10
+
+    cmp al, 13
+    je .newline
+
+    mov [di], al
+    inc di
+    dec cx
+
+    jmp .loop
+
+.newline:
+    mov al, 10
+    int 0x10
+.done:
+    mov byte [di], 0x00
+    ret
+
+
+string_cmp:  ; di: first string, si: second string, cx: length
+    .loop:
+        mov al, [si]
+        cmp [di], al
+        jne .done
+
+        cmp byte [di], 0
+        je .done
+
+        loop .loop
+
+        cmp al, al
+    .done:
+        ret
